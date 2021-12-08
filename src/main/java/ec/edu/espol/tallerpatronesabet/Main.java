b0836241fd4cfabd33ab458cbca1c536af6f440e
davidperjac <diperez@espol.edu.ec>
2021-12-08 09:28:09 -0500

    first commit

---
+++ src/main/java/ec/edu/espol/tallerpatronesabet/Main.java
@@ -0,0 +1,13 @@
+package ec.edu.espol.tallerpatronesabet;
+
+/**
+ *
+ * @author davidperez
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
